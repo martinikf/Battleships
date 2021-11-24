5ca65cf8890e5859afd507626f28d504f508e733
Filip Martiník <martinik.filip01@gmail.com>
2021-11-24 12:08:56 +0100

    Create occupies

--- src/Battleships/Ship.java
+++ src/Battleships/Ship.java
@@ -6,7 +6,15 @@ public abstract class Ship {
     protected byte rotation;
     protected Position start;
 
-    public Ship(int row, int col){
+    public Ship(int row, int col, byte rotation){
         start = new Position(row, col);
+        this.rotation = rotation;
+        createOccupies();
     }
+
+    protected abstract boolean createOccupies();
+
+    public Position[] getOccupies() { return occupies; }
+    public Position getStart(){ return start;}
+    public byte getRotation(){return rotation;}
 }
--- src/Battleships/StraightShip.java
+++ src/Battleships/StraightShip.java
@@ -1,17 +1,37 @@
 package Battleships;
 
+import jdk.jshell.spi.ExecutionControl;
+
 public class StraightShip extends Ship{
 
     private final int length;
 
-    public StraightShip(int row, int col, int length){
-        super(row, col);
+    public StraightShip(int row, int col, byte rotation, int length) throws ExecutionControl.NotImplementedException {
+        super(row, col, rotation);
         this.length = length;
-        occupies = new Position[getLength()];
-        occupies[0] = start;
+        if(!createOccupies()){
+            throw new ExecutionControl.NotImplementedException("Lod nelze umístit");
+        }
     }
 
     public int getLength() {
         return length;
+    }
+
+    @Override
+    protected boolean createOccupies() {
+        occupies = new Position[getLength()];
+        occupies[0] = start;
+
+        for(int i = 1; i < getLength(); i++){
+            if(rotation == 0){
+                occupies[i] = new Position(getStart().getRow(), getStart().getCol() + i);
+            }
+
+            else if(rotation == 1){
+                occupies[i] = new Position(getStart().getRow() + i, getStart().getCol());
+            }
+        }
+        return true;
     }
 }
